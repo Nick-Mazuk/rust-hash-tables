9622955bfe368df5a39573d03344f049b246e2e3
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-08-28 22:12:21 -0700

    fix rng

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use rand::random;
+use rand::prelude::*;
+use rand::thread_rng as rng;
 use std::collections::HashSet;
 
 struct Data<T> {
@@ -27,33 +28,34 @@ fn create_string_data(n: usize) -> Data<String> {
     Data { set, vec }
 }
 
+fn random(size: f64) -> usize {
+    (rng().gen::<f64>() * size) as usize
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
-    let size = 1000;
+    let size = 100000;
     let number_data = create_number_data(size);
     let string_data = create_string_data(size);
+    let size_float = size as f64;
 
     c.bench_function("number vec", |b| {
         b.iter(|| {
-            number_data.vec.contains(&(random::<usize>() * size));
+            number_data.vec.contains(&(random(size_float)));
         })
     });
     c.bench_function("number set", |b| {
         b.iter(|| {
-            number_data.set.contains(&(random::<usize>() * size));
+            number_data.set.contains(&(random(size_float)));
         })
     });
     c.bench_function("string vec", |b| {
         b.iter(|| {
-            string_data
-                .vec
-                .contains(&format!("{}", random::<usize>() * size));
+            string_data.vec.contains(&format!("{}", random(size_float)));
         })
     });
     c.bench_function("string set", |b| {
         b.iter(|| {
-            string_data
-                .set
-                .contains(&format!("{}", random::<usize>() * size));
+            string_data.set.contains(&format!("{}", random(size_float)));
         })
     });
 }
